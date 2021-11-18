510c90969d58f67e4004b64cf4eff0a912b66d09
Vladyslav <vladickbaraza@gmail.com>
2021-11-18 23:13:28 +0100

    added tuits controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TuitsController } from './tuits/tuits.controller';
+import { TuitsService } from './tuits/tuits.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TuitsController],
+  providers: [AppService, TuitsService],
 })
 export class AppModule {}
